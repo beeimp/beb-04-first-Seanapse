f8f504aa216aa59c2644376f8f733d1175bdb7a7
BeeImp <zero9231@naver.com>
2022-06-16 02:12:01 +0900

    Feat: NFT 거래 컨트랙트 구현

--- hardhat/contracts/Erc721.sol
+++ hardhat/contracts/Erc721.sol
@@ -13,6 +13,15 @@ contract SeanapseNFT is ERC721URIStorage, Ownable, ERC721Enumerable {
 
     constructor() public ERC721("SeanapseNFT", "SNFT") {}
 
+    // NFT 가격 및 구매 여부 Structure
+    struct NftPrice {
+        bool available; // false: 구매 불가능, true: 구매 가능
+        uint256 price;
+    }
+
+    // NFT 가격 및 구매 여부 매핑
+    mapping(uint256 => NftPrice) nftPrices;
+
     function _beforeTokenTransfer(
         address from,
         address to,
@@ -56,6 +65,39 @@ contract SeanapseNFT is ERC721URIStorage, Ownable, ERC721Enumerable {
         _mint(recipient, newItemId);
         _setTokenURI(newItemId, tokenURI);
 
+        nftPrices[newItemId] = NftPrice({available: false, price: 0});
+
         return newItemId;
+    }
+
+    // NFT 판매 조회
+    function isAvailable(uint256 _tokenId) public view returns (bool) {
+        return nftPrices[_tokenId].available;
+    }
+
+    // NFT 가격 조회
+    function isAvailable(uint256 _tokenId) public view returns (uint256) {
+        require(nftPrices[_tokenId].available);
+        return nftPrices[_tokenId].price;
+    }
+
+    // NFT 판매 함수
+    function sellNFT(
+        address _address,
+        uint256 _tokenId,
+        uint256 _price
+    ) public {
+        address owner = ownerOf(_tokenId);
+        require(owner == _address);
+        nftPrices[_tokenId].available = true;
+        nftPrices[_tokenId].price = _price;
+    }
+
+    // NFT 구매 함수
+    function buyNFT(uint _tokenId) public payable {
+        require(nftPrices[_tokenId].price == msg.value);
+        address payable owner = payable(ownerOf(_tokenId));
+        owner.transfer(nftPrices[_tokenId].price);
+        transferFrom(owner, msg.sender, _tokenId);
     }
 }
